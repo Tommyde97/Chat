cd0b275dba038b4be76ce24ecbcc2724a45f356e
Tommy <tommyde97@hotmail.com>
2023-06-14 14:41:52 -0400

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Tomas D. De Andrade Gomes on 6/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
